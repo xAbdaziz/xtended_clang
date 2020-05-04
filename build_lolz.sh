f749ce1f945613b4ce19adb53ac6298458c9e568
xawlw <abdulazizawlw@gmail.com>
2020-05-04 12:08:32 +0300

    Adapt it for me
    Thonks Jprimero15

--- build_lolz.sh
+++ build_lolz.sh
@@ -7,8 +7,8 @@ export DEBIAN_FRONTEND=noninteractive
 LOLZ_DIR="$(pwd)"
 
 # Github info
-git config --global user.name "Jprimero15"
-git config --global user.email "jprimero155@gmail.com"
+git config --global user.name "xawlw"
+git config --global user.email "abdulazizawlw@gmail.com"
 
 # Inlined function to post a message
 export BOT_MSG_URL="https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage"
@@ -25,19 +25,25 @@ lolz_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
 builder_commit="$(git rev-parse HEAD)"
 
 # Send a notificaton to TG
-tg_post_msg "<b>LOLZ Clang Compilation Started</b>%0A<b>Date: </b><code>$lolz_friendly_date</code>%0A<b>CLANG Script Commit: </b><code>$builder_commit</code>%0A"
+tg_post_msg "<b>xawlw's Clang Compilation Started</b>%0A<b>Date: </b><code>$lolz_friendly_date</code>%0A<b>CLANG Script Commit: </b><code>$builder_commit</code>%0A"
 
 # Build LLVM
 tg_post_msg "<code>Building LLVM...</code>"
 ./build-llvm.py \
-    --clang-vendor "LOLZ" \
-    --targets "ARM;AArch64;X86" \
-    --shallow-clone \
-    --pgo
+	--clang-vendor "xawlw's" \
+	--targets "ARM;AArch64;X86" \
+	--shallow-clone \
+	--pgo \
+	--lto thin \
+	--m skylake \
+    --build-stage1-only \
+    --install-stage1-only
 
 # Build binutils
 tg_post_msg "<code>Building Binutils...</code>"
-./build-binutils.py --targets arm aarch64 x86_64
+./build-binutils.py \
+    --targets arm aarch64 x86_64 \
+    -m skylake
 
 # Remove unused products
 tg_post_msg "<code>Removing Unused Products...</code>"
@@ -67,22 +73,22 @@ popd
 binutils_ver="2.34"
 clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
 
-# Push to GitHub LOLZ clang builds repo
-tg_post_msg "<code>Pushing New LOLZ Clang Build to Github Repo...</code>"
-git clone "https://Jprimero15:$GITHUB_TOKEN@github.com/Jprimero15/lolz_clang.git" lolz_repo
-pushd lolz_repo
+# Push to GitHub xawlw's clang builds repo
+tg_post_msg "<code>Pushing New xawlw's Clang Build to Github Repo...</code>"
+git clone "https://xawlw:$GITHUB_TOKEN@github.com/xawlw/my_clang.git" --depth=1 my_repo
+pushd my_repo
 rm -fr ./*
 cp -r ../install/* .
-echo "$clang_version-LOLZClang-$lolz_date" >VERSION
+echo "$clang_version-xawlw's_Clang-$lolz_date" >VERSION
 git add .
 git commit -m "Update to $lolz_date Build
 
 LLVM commit: $llvm_commit_url
 binutils version: $binutils_ver
-Builder commit: https://github.com/Jprimero15/lolz-clang-build/commit/$builder_commit"
-git push
+Builder commit: https://github.com/xawlw/xawlw-clang-build/commit/$builder_commit"
+git push -f
 lolzclang_commit="$(git rev-parse HEAD)"
-lolzclang_commit_url="https://github.com/Jprimero15/lolz_clang/commit/$lolzclang_commit"
+lolzclang_commit_url="https://github.com/xawlw/my_clang/commit/$lolzclang_commit"
 popd
 
-tg_post_msg "<b>LOLZ Clang Compilation Finished</b>%0A<b>Clang Version: </b><code>$clang_version</code>%0A<b>LLVM Commit: </b><code>$llvm_commit_url</code>%0A<b>Binutils Version: </b><code>$binutils_ver</code>%0A<b>LOLZ Clang Commit: </b><code>$lolzclang_commit_url</code>"
+tg_post_msg "<b>xawlw's Clang Compilation Finished</b>%0A<b>Clang Version: </b><code>$clang_version</code>%0A<b>LLVM Commit: </b><code>$llvm_commit_url</code>%0A<b>Binutils Version: </b><code>$binutils_ver</code>%0A<b>xawlw's Clang Commit: </b><code>$lolzclang_commit_url</code>"
