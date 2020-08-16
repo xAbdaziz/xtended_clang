8a275d9d64758246be4e48739f9d3beb04f1a55f
xawlw <abdulazizawlw@gmail.com>
2020-08-16 10:10:30 +0200

    Adapt to Xtended

--- build_xtended.sh
+++ build_xtended.sh
@@ -25,25 +25,25 @@ lolz_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
 builder_commit="$(git rev-parse HEAD)"
 
 # Send a notificaton to TG
-tg_post_msg "<b>xawlw's Clang Compilation Started</b>%0A<b>Date: </b><code>$lolz_friendly_date</code>%0A<b>CLANG Script Commit: </b><code>$builder_commit</code>%0A"
+tg_post_msg "<b>Xtended Clang Compilation Started</b>%0A<b>Date: </b><code>$lolz_friendly_date</code>%0A<b>CLANG Script Commit: </b><code>$builder_commit</code>%0A"
 
 # Build LLVM
 tg_post_msg "<code>Building LLVM...</code>"
 ./build-llvm.py \
-	--clang-vendor "xawlw's" \
-	--targets "ARM;AArch64;X86" \
+	--clang-vendor "Xtended" \
+	--targets "ARM;AArch64" \
 	--shallow-clone \
 	--pgo \
-	--lto thin \
-	--m skylake \
+	--lto full \
+	--m haswell \
     --build-stage1-only \
     --install-stage1-only
 
 # Build binutils
 tg_post_msg "<code>Building Binutils...</code>"
 ./build-binutils.py \
-    --targets arm aarch64 x86_64 \
-    -m skylake
+    --targets arm aarch64 \
+    -m haswell
 
 # Remove unused products
 tg_post_msg "<code>Removing Unused Products...</code>"
@@ -74,21 +74,21 @@ binutils_ver="2.34"
 clang_version="$(install/bin/clang --version | head -n1 | cut -d' ' -f4)"
 
 # Push to GitHub xawlw's clang builds repo
-tg_post_msg "<code>Pushing New xawlw's Clang Build to Github Repo...</code>"
-git clone "https://xawlw:$GITHUB_TOKEN@github.com/xawlw/my_clang.git" --depth=1 my_repo
+tg_post_msg "<code>Pushing New Xtended Clang Build to Github Repo...</code>"
+git clone "https://xawlw:$GITHUB_TOKEN@github.com/Project-Xtended/prebuilts_clang_host_linux-x86_clang-xtended.git" --depth=1 my_repo
 pushd my_repo
 rm -fr ./*
 cp -r ../install/* .
-echo "$clang_version-xawlw's_Clang-$lolz_date" >VERSION
+echo "$clang_version-Xtended_Clang-$lolz_date" >VERSION
 git add .
 git commit -m "Update to $lolz_date Build
 
 LLVM commit: $llvm_commit_url
 binutils version: $binutils_ver
-Builder commit: https://github.com/xawlw/xawlw-clang-build/commit/$builder_commit"
+Builder commit: https://github.com/xawlw/xtended_clang/commit/$builder_commit"
 git push -f
 lolzclang_commit="$(git rev-parse HEAD)"
-lolzclang_commit_url="https://github.com/xawlw/my_clang/commit/$lolzclang_commit"
+lolzclang_commit_url="https://github.com/Project-Xtended/prebuilts_clang_host_linux-x86_clang-xtended/commit/$lolzclang_commit"
 popd
 
-tg_post_msg "<b>xawlw's Clang Compilation Finished</b>%0A<b>Clang Version: </b><code>$clang_version</code>%0A<b>LLVM Commit: </b><code>$llvm_commit_url</code>%0A<b>Binutils Version: </b><code>$binutils_ver</code>%0A<b>xawlw's Clang Commit: </b><code>$lolzclang_commit_url</code>"
+tg_post_msg "<b>Xtended Clang Compilation Finished</b>%0A<b>Clang Version: </b><code>$clang_version</code>%0A<b>LLVM Commit: </b><code>$llvm_commit_url</code>%0A<b>Binutils Version: </b><code>$binutils_ver</code>%0A<b>Xtended Clang Commit: </b><code>$lolzclang_commit_url</code>"
